4f2f3eb3536a13e2a90aa61c84c8094cc6445180
sylabtv <sylab.studio@gmail.com>
2026-02-04 10:09:16 +0100

    jour 7

--- correction-hooks/App.jsx
+++ correction-hooks/App.jsx
@@ -4,6 +4,7 @@ import Post from "./Post.jsx";
 import "./App.css";
 
 function App() {
+  // setState charger l'etat initial
   const [state, setState] = useState(
     JSON.parse(localStorage.getItem("blogState")),
   );
@@ -27,29 +28,32 @@ function App() {
     }, []);*/
 
   useEffect(() => {
+    // sauvegarder la state avec localStorage
     localStorage.setItem("blogState", JSON.stringify(state));
 
     return () => {};
   }, [state]);
-
+  // nouveau states avec post
   function showPost(post) {
     let newState = { page: "post", post: post };
     setState(newState);
   }
-
+  // retour a la page blog
   function backToBlog(event) {
     event.preventDefault();
     let newState = { page: "blog", post: null };
     setState(newState);
   }
-
+  // si la state est sur le blog
   if (state.page === "blog") {
     return (
       <>
+        {/* afficher blog  */}
         <Blog showPost={showPost} />
       </>
     );
   } else {
+    // sinon afficher bouton retour blog et post
     return (
       <>
         <a href="index.html" className="back-link" onClick={backToBlog}>
--- correction-hooks/Blog.jsx
+++ correction-hooks/Blog.jsx
@@ -3,13 +3,15 @@ import PostCard from "./PostCard.jsx";
 import data from "./data.js";
 
 const Blog = (props) => {
+  // state de post et son etat initial
   const [posts, setPosts] = useState([]);
-
   useEffect(() => {
+    // setpost modifie le state
     setPosts(data);
   }, []);
 
   return (
+    // la grid
     <div className="blog-grid">
       {posts.map((post, index) => (
         <PostCard key={index} post={post} showPost={props.showPost} />
